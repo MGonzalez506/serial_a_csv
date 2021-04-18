40163e6d6f2e15fb7b239d85c75e306661370598
Miguel Gonzalez <MGonzalez506@users.noreply.github.com>
2021-04-18 16:57:18 -0600

    Actualizado "guardar_en_csv.py"
    Se actualizó el documento "guardar_en_csv.py" para guardar los datos con la hora de la zona horaria donde usted se encuentre.
    
    Utilizando la variable "zona_horaria" se puede converte la hora UTC a esa zona horaria

--- guardar_en_csv.py
+++ guardar_en_csv.py
@@ -18,19 +18,26 @@ import sys
 import os
 import pytz
 import csv
+import pytz
+from pytz import timezone
 from datetime import datetime
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 direccion_del_documento = str(THIS_FOLDER + ("/Documentos_CSV/Fecha_de_doc_"))
 
+zona_horaria = 'America/Regina'
+formato_de_timestamp = "%Y-%m-%d %H:%M:%S.%f %Z%z"
+
 def save_buffer(in_str):
-	fch = datetime.utcnow()
+	utc_ahora = datetime.now(pytz.timezone('UTC'))
 	vector_recibido = in_str.split(',')
-	print(fch.strftime('%Y-%m-%d %H:%M:%S.%f %Z') + str(vector_recibido))
-	_dir = direccion_del_documento + fch.strftime('%Y') + "_" + fch.strftime('%m') + "_" + fch.strftime('%d') + ".csv"
+	fecha = utc_ahora.astimezone(pytz.timezone(zona_horaria))
+	print(fecha)
+	print("\t\t" + str(vector_recibido))
+	_dir = direccion_del_documento + fecha.strftime('%Y') + "_" + fecha.strftime('%m') + "_" + fecha.strftime('%d') + ".csv"
 	if os.path.isfile(_dir):
 		save_vector = list()
-		save_vector.append(fch)
+		save_vector.append(fecha)
 		for x in vector_recibido: save_vector.append(x)
 		with open(_dir, 'a', newline='') as d:
 			escribir = csv.writer(d)
